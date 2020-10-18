2c33ed14e2da435c4e303cb0526a523e06d87de2
dumidum <dumidumadura@gmail.com>
2020-10-18 07:09:26 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 class App extends React.Component {
   render() {
     return (
@@ -12,6 +12,7 @@ class App extends React.Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
