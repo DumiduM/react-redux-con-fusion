5302be9b975131e96fe75029cab5e4f1f2359339
Dumindu De Silva <Dumindu@CreativeSoftware.com>
2021-03-28 23:16:10 +0530

    fetch from server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Modal, ModalHeader, Modal
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -12,7 +13,7 @@ function RenderDish({ dish }) {
   return (
     // <div className="col-12 col-md-5 m-1">
     <Card>
-      <CardImg src={dish.image} alt={dish.name} />
+      <CardImg top src={baseUrl + dish.image} alt={dish.name} />
       <CardBody>
         <CardTitle>{dish.name}</CardTitle>
         <CardText>{dish.description}</CardText>
@@ -70,35 +71,35 @@ const DishDetail = (props) => {
     );
   }
   else if (props.dish != null) {
-      return (
-        <div className="container">
-          <div className="row">
-            <Breadcrumb>
-              <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-            </Breadcrumb>
-            <div className="col-12">
-              <h3>{props.dish.name}</h3>
-              <hr />
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              <RenderDish dish={props.dish} />
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              <RenderComments comments={props.comments}
-                addComment={props.addComment}
-                dishId={props.dish.id}
-              />
-            </div>
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
           </div>
         </div>
-      );
-    }
-    else {
-      return (<div></div>)
-    }
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments comments={props.comments}
+              addComment={props.addComment}
+              dishId={props.dish.id}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  else {
+    return (<div></div>)
+  }
 }
 
 class CommentForm extends Component {
